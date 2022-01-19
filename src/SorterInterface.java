8c3e25fcd7f01dd7dd45a09c5c873a4e66f47fc4
JoksimovicM <76094121+JoksimovicM@users.noreply.github.com>
2022-01-19 09:51:42 +0100

    Add files via upload

---
+++ src/SorterInterface.java
@@ -0,0 +1,17 @@
+/**
+ * @author Marko Joksimovic
+ * @version 1.0
+ * @date
+ */
+public interface SorterInterface {
+
+    public void sort(int[] zahlen);
+
+    public int getSpeicherbedarf();
+
+    public long getTime();
+
+    public int anzVergleiche();
+
+    public int anzahlSchreibzugriffe();
+}
